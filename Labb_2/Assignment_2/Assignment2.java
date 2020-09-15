643ca7297009e0e3d8bfa25cc1741810a9049923
Adeel Hussain <60479388+Adde-h@users.noreply.github.com>
2020-09-15 10:17:00 +0200

    Created Assignment 1-3

---
+++ Labb_2/Assignment_2/Assignment2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Assignment2 
+{
+    static void printArray(int arraySize, int data[]) 
+    {  
+        for (int i = 0; i < arraySize; i++)
+        {
+            System.out.print("[" + data[i] + "], "); 
+        }  
+        System.out.println();
+    } 
+
+    public static void insertionsort(int arraySize, int data[])
+    {
+        int temp = 0;
+        int swapCounter = 0;
+        for(int i = 1; i < arraySize; i++)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                if(data[j] < data [j-1])
+                {
+                    temp = data[j-1];
+                    data[j-1] = data[j];
+                    data[j] = temp;
+                    swapCounter++;
+                }
+                else
+                {
+                    break;
+                }
+                printArray(arraySize, data);
+            }
+        }
+                System.out.println("Amount of swaps: " + swapCounter);
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter the size of the array: ");
+        Scanner indata = new Scanner(System.in);
+
+        int arraySize = indata.nextInt();
+        System.out.println("Enter the " + arraySize + " numbers to be sorted");
+        int[] data = new int[arraySize];
+        
+        for(int i = 0; i <= arraySize-1; i++)
+        {
+            data[i] = indata.nextInt();
+        }
+        
+        insertionsort(arraySize,data);
+
+        indata.close();
+    }
+}
